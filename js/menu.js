383af371fdaa0ff818a17d4a0738d61c33649ad8
Ram0s-bit <lucas.ramos.lr243@gmail.com>
2023-06-25 17:40:43 -0300

    organizando html e js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 // Toggle do menu responsivo
-var menuToggle = document.getElementById("menu-toggle");
-var menu = document.getElementById("menu");
+let menuToggle = document.getElementById("menu-toggle");
+let menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", function() {
   menu.classList.toggle("active");
