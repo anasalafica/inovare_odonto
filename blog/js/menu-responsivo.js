05ddb36c915308b34d59f07d9ae2b1649741bfbd
Thiago Henrique Domingues <dominguesthiagohenrique7@gmail.com>
2023-07-14 01:39:00 -0300

    atualizacao

--- blog/js/menu-responsivo.js
+++ blog/js/menu-responsivo.js
@@ -1,9 +1,39 @@
-let show = true
- 
-const menuSection = document.querySelector(".menu-responsivo-section")
-const menuBurguer = menuSection.querySelector(".menu-burguer")
+document.querySelector('.navbar__toggle').addEventListener('click',function() {
+    this.classList.toggle('open')
 
-menuBurguer.addEventListener("click", () => {
-    menuSection.classList.toggle("on", show)
-    show = !show
+    document.querySelector('.navbar__menu').classList.toggle('open')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+// let show = true
+ 
+// const menuSection = document.querySelector(".menu-responsivo-section")
+// const menuBurguer = menuSection.querySelector(".menu-burguer")
+
+// menuBurguer.addEventListener("click", () => {
+//     menuSection.classList.toggle("on", show)
+//     show = !show
+// })
+
+
+// const menuDiv = document.getElementById('menu-mobile')
+// const btnAnimar = document.getElementById('btn-menu-burguer')
+
+// menuDiv.addEventListener('click', animar2)
+
+// function animar2(){
+//     menuDiv.classList.toggle('funcionando')
+    
+
+//     console.log('animar')
+// }
